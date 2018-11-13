a8dc692ef5c6e6ed08193b77da9578e8a0eb21bf
bobboy65 <scmikeyt@yahoo.com>
2018-11-12 22:07:48 -0500

    initialized repo

---
+++ ECE4900/WeightSensorGUI/weightSensorGui.py
@@ -0,0 +1,13 @@
+#####################
+# ECE 4900 - Team 3         #
+#                                          #
+# File: weightSensorGui.py #
+####################
+
+from guizero import App
+
+weightGui = App(title="Weight Sensor");
+
+
+
+app.display()
---
+++ ECE4900/test.py
@@ -0,0 +1,5 @@
+
+git clone https://github.com/tatobari/hx711py
+
+cd hx711py
+sudo nano example.py
