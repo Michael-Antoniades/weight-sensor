2bf3e4d73dbdc9e8cf61322f600ca5f5f0898fff
bobboy65 <scmikeyt@yahoo.com>
2018-11-27 22:23:02 -0500

    changes...

--- ECE4900/WeightSensorGUI/weightSensorGui.py
+++ ECE4900/WeightSensorGUI/weightSensorGui.py
@@ -5,36 +5,65 @@
 ####################
 
 from guizero import App, Text, TextBox, PushButton
+import subprocess as sub
+import os
+import signal
+#from example import val
 
+#print(val,"test")
 # Globals
-DUMMY_SENSOR_OVER = 360.0
-DUMMY_SENSOR_UNDER = 20.0
 
+DUMMY_SENSOR_OVER = 200
+DUMMY_SENSOR_UNDER = 200
 
 # Functions
 def weight_set_success():
 	weight_enter_message.value = "Weight Successfully Set: " + user_weight_box.value + " lbs\n"
+	#print(val)
 
-	if (DUMMY_SENSOR_OVER > 0.2 * int(user_weight_box.value)): # over case
+	if (DUMMY_SENSOR_OVER > int(user_percent_box.value)/100 * int(user_weight_box.value)): # over case
 		gui.bg = "red"
-	elif (DUMMY_SENSOR_UNDER <= 0.2 * int(user_weight_box.value)): # under case
+	elif (DUMMY_SENSOR_UNDER <= int(user_percent_box.value)/100 * int(user_weight_box.value)): # under case
 		gui.bg = "lightgreen"
 
-def over_or_under():
-	if (DUMMY_SENSOR_OVER > 0.2 * user_weight_box.value): # over case
-		over_alert = Text(gui, text="Too fat!", size=16, font="Times New Roman", color="red")
-	elif (DUMMY_SENSOR_UNDER <= 0.2 * user_weight_box.value): # under case
-		under_alert = Text(gui, text="Too skinny!", size=16, font="Times New Roman", color="green")
+#percent set success
+def percent_set_success():
+	weight_percentage_message.value = "Percentage Successfully Set: " + user_percent_box.value + " %\n"	
 
+
+	if (DUMMY_SENSOR_OVER > int(user_percent_box.value)/100 * int(user_weight_box.value)): # over case
+		gui.bg = "red"
+	elif (DUMMY_SENSOR_UNDER <= int(user_percent_box.value)/100 * int(user_weight_box.value)): # under case
+		gui.bg = "lightgreen"
+
+#def over_or_under():
+#	if (DUMMY_SENSOR_OVER > 0.2 * user_weight_box.value): # over case
+#		over_alert = Text(gui, text="Too fat!", size=8, font="Times New Roman", color="red")
+#	elif (DUMMY_SENSOR_UNDER <= 0.2 * user_weight_box.value): # under case
+#		under_alert = Text(gui, text="Too skinny!", size=8, font="Times New Roman", color="green")
+
+
+# Fork keyboard process
+proc = sub.Popen("matchbox-keyboard", shell=True, stdout=sub.PIPE, stderr=sub.PIPE)
 
 # Main GUI Script
-gui = App(title="Weight Sensor", height=800, width=800)
+gui = App(title="Weight Sensor", height=240, width=320)
 
-welcome_message = Text(gui, text="Welcome to Team 3's Weight Sensor", size=40, font="Times New Roman")
-weight_enter_message = Text(gui, text="Please Enter Your Weight Below\n", size=26, font="Times New Roman", color="blue")
+welcome_message = Text(gui, text="Welcome to Team 3's Weight Sensor", size=16, font="Times New Roman")
+weight_enter_message = Text(gui, text="Please Enter Your Weight Below\n", size=13, font="Times New Roman", color="blue")
 
 user_weight_box = TextBox(gui, width=30)
 set_button = PushButton(gui, text="Set Weight (lbs)", command=weight_set_success)
 
+#Weight Percentage
+weight_percentage_message = Text(gui, text="Please Enter Weight Percentage\n", size=13, font="Times New Roman", color="blue")
+
+user_percent_box = TextBox(gui, width=30)
+set_button = PushButton(gui, text="Set Percentage (0-100%)", command=percent_set_success)
+
+#end
+
 gui.display()
+
+os.killpg(os.getpgid(proc.pid), signal.SIGTERM)
 
